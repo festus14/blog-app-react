1fcdf3e630782b3959f5755399d333ffb1a53a7f
Festus Omole <festusomole14@gmail.com>
2020-03-25 04:54:01 +0100

    Blog completed

---
+++ src/containers/Blog/Posts/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import { Route } from 'react-router-dom';
+
+import Post from '../../../components/Post';
+import './styles.css';
+import FullPost from '../FullPost';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        console.log( this.props );
+        axios.get( '/posts' )
+            .then( response => {
+                const posts = response.data.slice( 0, 4 );
+                const updatedPosts = posts.map( post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                } );
+                this.setState( { posts: updatedPosts } );
+                // console.log( response );
+            } )
+            .catch( error => {
+                console.log( error );
+                // this.setState({error: true});
+            } );
+    }
+
+    postSelectedHandler = ( id ) => {
+        // this.props.history.push({pathname: '/posts/' + id});
+        this.props.history.push( '/posts/' + id );
+    }
+
+    render () {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+        if ( !this.state.error ) {
+            posts = this.state.posts.map( post => {
+                return (
+                    // <Link to={'/posts/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler( post.id )} />
+                    // </Link>
+                );
+            } );
+        }
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost} />
+            </div>
+        );
+    }
+}
+
+export default Posts;
---
+++ src/containers/Blog/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, NavLink, Switch } from 'react-router-dom';
+
+import './styles.css';
+import Posts from './Posts';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost');
+});
+
+class Blog extends Component {
+    state = {
+        auth: true
+    }
+
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route path="/" component={Posts} /> */}
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default Blog;
